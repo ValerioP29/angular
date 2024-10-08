1581d4901a0a1db71f1353e5ddd0239fb2c80521
Valerio Persiani <persianivalerio0@gmail.com>
2024-10-08 16:43:01 +0200

    first trial of angular component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InputComponent } from './component/input/input.component';
+import { TitleComponent } from './textComponents/title/title.component';
+import { ButtonComponent } from './buttons/button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputComponent,
+    TitleComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
